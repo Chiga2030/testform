1d05809c16d1f0ca1d049e751f0396a8b353897d
Anton <chiga2030@gmail.com>
2021-07-22 22:32:14 +0300

    created structure of form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,9 @@
+import styles from './Form.module.css';
+
+const Form = () => (
+  <form className={styles.form}>
+    lol
+  </form>
+);
+
+export default Form;
---
+++ src/components/FormWrapper/FormWrapper.jsx
@@ -0,0 +1,11 @@
+import styles from './FormWrapper.module.css';
+
+import Form from '../Form/Form';
+
+const FormWrapper = () => (
+    <div className={styles.wrapper}>
+      <Form />
+    </div>
+  );
+
+export default FormWrapper;
