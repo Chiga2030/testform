226277fc99a0d9b1a2dc800155c7285bc7ab306a
Anton <chiga2030@gmail.com>
2021-07-23 23:27:46 +0300

    added state

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
+
 import styles from './Form.module.css';
 
 import Info from '../Info/Info';
 import ProductList from '../ProductList/ProductList';
 
-const Form = () => (
-  <form className={styles.form}>
-    <Info
-      styles={styles}
-    />
-    <ProductList styles={styles} />
-  </form>
-);
+const defaultProducts = [
+  {
+    id: 1,
+    name: 'Product 1',
+  },
+];
+const defaultPrice = 24.99;
+
+const Form = () => {
+  const [products, setProducts] = useState(defaultProducts);
+  const [price, setPrice] = useState(defaultPrice);
+
+  return (
+    <form className={styles.form}>
+      <Info
+        styles={styles}
+      />
+      <ProductList
+        styles={styles}
+        products={products}
+      />
+    </form>
+  )
+};
 
 export default Form;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,39 @@
 const ProductList = ({
   styles,
+  products,
   ...props
 }) => (
-  <>
-    <h2 className={styles.title}>Product 1</h2>
-  </>
+  <div className={styles.blockMargins}>
+    {products.map(
+      product => (
+        <div className={styles.blockMargins}>
+          <h2 className={styles.title}>
+            {product.name}
+          </h2>
+          <label>
+            <p className={styles.subtitle}>
+              Enter main keyword for the product
+            </p>
+            <input
+              className={styles.input}
+              placeholder="for example, sylicon wine cup"
+              type="text"
+            />
+          </label>
+          <label>
+            <p className={styles.subtitle}>
+              Enter link to the similar product as a reference
+            </p>
+            <input
+              className={styles.input}
+              placeholder="https://..."
+              type="text"
+            />
+          </label>
+        </div>
+      )
+    )}
+  </div>
 );
 
 export default ProductList;
