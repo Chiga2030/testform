5b223fe1dc20e9aa3608e8a6b447c1b0f47e7e6e
Anton <chiga2030@gmail.com>
2021-07-23 13:34:33 +0300

    added poles for Info component

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -5,6 +5,16 @@ const Info = () => (
     <h2 className={styles.title}>
       Info
     </h2>
+    <label>
+      <p className={styles.subtitle}>
+        Enter your email address
+      </p>
+      <input
+        className={styles.input}
+        placeholder="team@checkforpatent.com"
+        type="email"
+      />
+    </label>
   </>
 );
 
