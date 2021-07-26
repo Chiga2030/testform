36aad13f535b6835f5d53279161fa25cff2825bf
Anton <chiga2030@gmail.com>
2021-07-26 11:51:39 +0300

    rebased jsx template to seporate component

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,3 +1,5 @@
+import Product from '../Product/Product';
+
 const ProductList = ({
   styles,
   products,
@@ -6,31 +8,10 @@ const ProductList = ({
   <div className={styles.blockMargins}>
     {products.map(
       product => (
-        <div className={styles.blockMargins}>
-          <h2 className={styles.title}>
-            {product.name}
-          </h2>
-          <label>
-            <p className={styles.subtitle}>
-              Enter main keyword for the product
-            </p>
-            <input
-              className={styles.input}
-              placeholder="for example, sylicon wine cup"
-              type="text"
-            />
-          </label>
-          <label>
-            <p className={styles.subtitle}>
-              Enter link to the similar product as a reference
-            </p>
-            <input
-              className={styles.input}
-              placeholder="https://..."
-              type="text"
-            />
-          </label>
-        </div>
+        <Product
+          styles={styles}
+          product={product}
+        />
       )
     )}
   </div>
