a043845a20c0b9d2763b52e06ef9fe271a76b806
Anton <chiga2030@gmail.com>
2021-07-27 16:07:16 +0300

    added components for adding more products

---
+++ src/components/SelectCountProd/SelectCountProd.jsx
@@ -0,0 +1,12 @@
+import Header from '../Header/Header';
+
+const SelectCountProd = () => (
+  <>
+    <Header
+      title="Adding more products"
+      subtitle="The more items you check, the better the price."
+    />
+  </>
+);
+
+export default SelectCountProd;
