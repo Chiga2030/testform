82008f700cb461d594153c65eec9773f1caa5d4e
Anton <chiga2030@gmail.com>
2021-08-05 06:41:26 +0300

    modyfi produckList

--- src/components/ListProductItem/ListProductItem.jsx
+++ src/components/ListProductItem/ListProductItem.jsx
@@ -7,31 +7,33 @@ const ListProductItem = ({
   discount,
   ...props
 }) => (
-  <li className={props.styles.column}>
-    <input
-      className={props.styles.radio}
-      name="selectProduct"
-      type="radio"
-      value="5"
-    />
-    <div className={props.styles.description}>
-      <p className={classNames(
-        props.styles.title,
+  <li className={props.styles.withBorderBottom}>
+    <label className={props.styles.column}>
+      <input
+        className={props.styles.radio}
+        name="selectProduct"
+        type="radio"
+        value="5"
+      />
+      <div className={props.styles.description}>
+        <p className={classNames(
+          props.styles.title,
+          {
+            [props.styles.onlyTitle]: !discount,
+          },
+        )}>
+          {product} for {price} usd {
+            (priceForEach ? `/ ${priceForEach}$ for each` : '')
+          }
+        </p>
         {
-          [props.styles.onlyTitle]: !discount,
-        },
-      )}>
-        {product} for {price} usd {
-          (priceForEach ? `/ ${priceForEach}$ for each` : '')
-        }
-      </p>
-      {
-        (discount ?
-          (<p className={props.styles.subtitle}>
-            You safe ${discount}% on each patent check
-          </p>) : ''
-        )}
-    </div>
+          (discount ?
+            (<p className={props.styles.subtitle}>
+              You safe ${discount}% on each patent check
+            </p>) : ''
+          )}
+      </div>
+    </label>
   </li>
 );
 
